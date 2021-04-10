0c6dbdfbbc586e880ebc7e60687ce5acc4ca2d09
Camiladev1 <>
2021-04-10 22:39:35 +0000

    Cadastro de usuários

---
+++ SistemaCadastroUsuarios/CadastroDeUsuarios.java
@@ -0,0 +1,17 @@
+/* 
+1 - Você foi contratado para desenvolver o Sistema Único de Cadastro de Brasileiros. 
+Esse sistema é utilizado pela União para registrar todos os cidadãos brasileiros, junto de suas informações, como:
+  - Nome
+  - CPF
+  - Data de Nascimento
+  - Endereço
+  - Renda mensal
+
+Para que o sistema possa armazenar as informações dos brasileiros, é necessário permitir que essas informações sejam
+ armazenadas pelo sistema automaticamente em alguma estrutura de dados de sua escolha. */
+
+//adicionar automaticamente a uma lista.
+//conjunto? Interface set? Sim, usar o exemplo do Banco.
+public class CadastroDeUsuarios{
+  
+}
