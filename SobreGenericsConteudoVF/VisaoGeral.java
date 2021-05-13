25f10691128c766507179e1d389026b1a824d858
Camiladev1 <>
2021-05-13 01:25:43 +0000

    para VF AAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ SobreGenericsConteudoVF/VisaoGeral.java
@@ -0,0 +1,22 @@
+tipos genéricos: uma funcionarlidade um piuo mais avancada de orientação a obhetos // lista é um tipo generico
+
+
+tipo generico tipo que aceita subtipos deste mesmo tipo sosmente no momento de instanciacao / criacao deste tio especifico
+
+
+no Java tudo herda de Object
+
+dentro de uma lista podemos adicionar objetos  de qualquer tipo 
+
+
+nao mpode sert srring pq a lista aqui aceita qualuer coisa, ent deve ser Object., mas normalmente usamos lista dos memso tipo
+
+tipo generico eh como se fosse um paramnetro asscoaidao ao tip. o tipo generico / parametrizado  entre as diamenamtes<>)
+para definir os tipos e retoeno s do metodo da classe
+
+
+
+
+
+casting => conversão de tipo 
+
