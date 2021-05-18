16f70388428310af029069b07d838cbba6d048b3
Camiladev1 <>
2021-05-18 01:57:07 +0000

    Visão Geral Tipos Genéricos

--- SobreGenericsConteudoVF/VisaoGeral.java
+++ SobreGenericsConteudoVF/VisaoGeral.java
@@ -5,17 +5,18 @@ tipo generico tipo que aceita subtipos deste mesmo tipo sosmente no momento de i
 
 
 no Java tudo herda de Object
-
+tudo é instancia de alguma subclassse de Object
 dentro de uma lista podemos adicionar objetos  de qualquer tipo 
 
 
 nao mpode sert srring pq a lista aqui aceita qualuer coisa, ent deve ser Object., mas normalmente usamos lista dos memso tipo
 
-tipo generico eh como se fosse um paramnetro asscoaidao ao tip. o tipo generico / parametrizado  entre as diamenamtes<>)
-para definir os tipos e retoeno s do metodo da classe
+tipo generico é como se fosse um paramnetro associado ao tipo. 
+o tipo generico / parametrizadofica entre as diamenamtes<>)
+é usado para definir os tipos e retoeno s do metodo da classe
 
 
-
+Nos tipos genéricos não da pra usar tipo primitivo, ent n pode usar int, e é ai que usa o integer, pq ele é uma classe
 
 
 casting => conversão de tipo 
