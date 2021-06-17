11594684b30f4d8173deae18df62628f1f075de6
Camiladev1 <>
2021-06-17 00:00:49 +0000

    trabalho de VF

---
+++ trabalhoVFPOO/TipoDeVeiculo.java
@@ -0,0 +1,8 @@
+public enum TipoDeVeiculo { 
+  /*Implementar classe para cada tipo de veiculo, para cada uma ter suas próprias caracteristicas */
+
+    CARRO,
+    CARRO_ELETRICO,
+    BICICLETA
+
+}
